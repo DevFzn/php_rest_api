f82490f59d43eb72ce1699cc4ebda2c78a2e4c47
devfzn <dev_fzn@proton.me>
2023-03-24 18:32:35 -0300

    edited: pacientes.class.php y pacientes.php
    pacientes.class.php: creados atributos paciente y función post
    pacientes.php: codición POST

--- apirest_yt/clases/pacientes.class.php
+++ apirest_yt/clases/pacientes.class.php
@@ -5,6 +5,16 @@ require_once 'clases/respuestas.class.php';
 class pacientes extends conexion{
 
     private $table = "pacientes";
+    private $pacienteid = "";
+    private $dni = "";
+    private $nombre = "";
+    private $direccion = "";
+    private $codigo_postal = "";
+    private $genero = "";
+    private $telefono = "";
+    private $fecha_nacimiento = "0000-00-00";
+    private $correo = "";
+
 
     public function listaPacientes($pagina = 1){
         // paginador
@@ -24,6 +34,47 @@ class pacientes extends conexion{
     public function obtenerPaciente($id){
         $query = "SELECT * FROM " . $this->table . " WHERE PacienteId = '$id'";
         return parent::obtenerDatos($query);
+    }
+
+    public function post($json){
+        $_respuestas = new respuestas;
+        $datos = json_decode($json, true);
+        if (!isset($datos['nombre']) || !isset($datos['dni']) || !isset($datos['correo'])){
+            return $_respuestas->error_400();
+        } else {
+            $this->nombre = $datos['nombre'];
+            $this->dni = $datos['dni'];
+            $this->correo = $datos['correo'];
+            if (isset($datos['telefono'])){ $this->telefono = $datos['telefono']; }
+            if (isset($datos['direccion'])){ $this->direccion = $datos['direccion']; }
+            if (isset($datos['codigo_postal'])){ $this->codigo_postal = $datos['codigo_postal']; }
+            if (isset($datos['genero'])){ $this->genero = $datos['genero']; }
+            if (isset($datos['fecha_nacimiento'])){ $this->fecha_nacimiento = $datos['fecha_nacimiento']; }
+            $resp = $this->insertarPaciente();
+            if ($resp){
+                $respuesta = $_respuestas->response;
+                $respuesta['result'] = array(
+                    'pacienteid' => $resp
+                );
+                return $respuesta;
+            } else {
+                return $_respuestas->error_500();
+            }
+        }
+    }
+
+    private function insertarPaciente(){
+        $query = "INSERT INTO $this->table (DNI, Nombre, Direccion, CodigoPostal,
+                                            Telefono, Genero, FechaNacimiento, Correo)
+                    values ('$this->dni','$this->nombre','$this->direccion',
+                    '$this->codigo_postal','$this->telefono','$this->genero',
+                    '$this->fecha_nacimiento','$this->correo')";
+        $resp = parent::nonQueryId($query);
+        if ($resp){
+            return $resp;
+        } else {
+            return 0;
+        }
     }
 }
 
--- apirest_yt/pacientes.php
+++ apirest_yt/pacientes.php
@@ -5,20 +5,34 @@ require_once 'clases/pacientes.class.php';
 $_respuestas = new respuestas;
 $_pacientes = new pacientes;
 
-#header('Content-Type: text/plain; charset=utf-8');
 header('Content-Type: application/json; charset=utf-8');
 if ($_SERVER['REQUEST_METHOD'] == "GET"){
+
     if (isset($_GET["page"])){
         $pagina = $_GET["page"];
         $lista_pacientes = $_pacientes->listaPacientes($pagina);
         echo json_encode($lista_pacientes); 
+        http_response_code(200);
     } else if (isset($_GET["id"])){
         $pacienteid = $_GET["id"];
         $datos_paciente = $_pacientes->obtenerPaciente($pacienteid);
         echo json_encode($datos_paciente);
+        http_response_code(200);
     }
+
 } else if ($_SERVER['REQUEST_METHOD'] == "POST"){
-    echo "hola POST";
+    // recepción de datos
+    $postBody = file_get_contents("php://input");
+    // envio de datos al manejador
+    $datosArray = $_pacientes->post($postBody);
+    // devolucion de respuesta
+    if(isset($datosArray["result"]["error_id"])){
+        $responseCode = $datosArray["result"]["error_id"];
+        http_response_code($responseCode);
+    }else{
+        http_response_code(200);
+    }
+    echo json_encode($datosArray);
 
 } else if ($_SERVER['REQUEST_METHOD'] == "PUT"){
     echo "hola PUT";
